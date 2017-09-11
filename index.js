2e4ab074f3e5fea0c1b8111acb15b710ee474290
Salvatore Laisa <moebiusmania@gmail.com>
2017-09-11 22:48:13 +0200

    add twitter endpoint

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ server.route({
         "/api/traffic",
         "/api/status",
         "/api/news",
+        "/api/twitter"
       ], 
       package: require('./package.json')
     });
@@ -83,6 +84,25 @@ server.route({
         return Object.assign({}, e, {line: _line});
       });
       reply(resp);
+    });
+  }
+});
+
+
+server.route({
+  method: 'GET',
+  path: '/api/twitter',
+  handler: (request, reply) => {
+    const selector = 'li.js-stream-item.stream-item.stream-item';
+    const schema = [{
+      text: 'div.js-tweet-text-container p@text',
+      time: 'div.stream-item-header span.js-relative-timestamp@text | trim',
+      url: 'a.tweet-timestamp.js-permalink@href'
+    }];
+    const stream = x('https://twitter.com/atm_informa', selector, schema ).stream();
+    stream.on('data', (data) => {
+      const json = JSON.parse(data.toString());
+      reply(json);
     });
   }
 });
