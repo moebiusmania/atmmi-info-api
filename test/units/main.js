dd232784cbe4136a9d59bd20834954e28ceadb97
Salvatore Laisa <moebiusmania@gmail.com>
2017-09-16 21:54:17 +0200

    add @std/esm module

---
+++ test/units/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import test from 'ava';
+import main from './../../src/routes/main.mjs';
+
+// test(t => {
+// 	t.is(main.status, 'Up & running');
+// });
+
+test('foo', t => {
+	t.pass();
+});
