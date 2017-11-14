6aa8abca31f81c9a1d3b16b2d7464199c52d8f9b
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-11-14 13:32:58 +0100

    setting the header Access-Control-Allow-Origin to *

--- src/server.mjs
+++ src/server.mjs
@@ -6,9 +6,11 @@ import news from './routes/news';
 import status from './routes/status';
 import traffic from './routes/traffic';
 import Hapi from 'hapi';
+import setHeader from 'hapi-set-header';
 import { PORT } from './constants';
 
 const server = new Hapi.Server();
+setHeader(server, 'Access-Control-Allow-Origin', '*');
 
 const ROUTES = [main, twitter, news, status, traffic];
 
