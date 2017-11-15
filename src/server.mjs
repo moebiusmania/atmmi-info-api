02c4ac1c6d3139e10e0f16c79bbe0006696468f5
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-11-15 14:34:18 +0100

    using port from environment for containerization

--- src/server.mjs
+++ src/server.mjs
@@ -7,14 +7,17 @@ import status from './routes/status';
 import traffic from './routes/traffic';
 import Hapi from 'hapi';
 import setHeader from 'hapi-set-header';
-import { PORT } from './constants';
+
+const PORT = process.env.PORT
+
+console.log("Configured port " + PORT);
 
 const server = new Hapi.Server();
 setHeader(server, 'Access-Control-Allow-Origin', '*');
 
 const ROUTES = [main, twitter, news, status, traffic];
 
-server.connection({ port: process.env.PORT });
+server.connection({ port: PORT });
 
 ROUTES.forEach( module => server.route(module) );
 
