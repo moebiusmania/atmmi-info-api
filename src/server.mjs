98979b5e45d1b302b4c92326ad5d6bc71aba7a4b
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-11-15 14:38:31 +0100

    bugfix

--- src/server.mjs
+++ src/server.mjs
@@ -7,17 +7,17 @@ import status from './routes/status';
 import traffic from './routes/traffic';
 import Hapi from 'hapi';
 import setHeader from 'hapi-set-header';
+import {PORT} from './configuration';
+const CONF_PORT = process.env.PORT || PORT;
 
-const PORT = process.env.PORT
-
-console.log("Configured port " + PORT);
+console.log("Configured port " + CONF_PORT);
 
 const server = new Hapi.Server();
-setHeader(server, 'Access-Control-Allow-Origin', '*');
 
 const ROUTES = [main, twitter, news, status, traffic];
 
-server.connection({ port: PORT });
+server.connection({ port: CONF_PORT });
+setHeader(server, 'Access-Control-Allow-Origin', '*');
 
 ROUTES.forEach( module => server.route(module) );
 
