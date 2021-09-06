d7dcbc48dd9cfb647f87966b066bfc372812615a
Salvatore Laisa <moebiusmania@gmail.com>
2021-09-06 16:56:57 +0200

    feat: adding missing functionalities from node.js version

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ type LineStatus struct {
 	Status string `json:"status"`
 }
 
-const DOMAIN string = "https://www.atm.it"
 const URL string = "https://www.atm.it/it/AtmNews/Pagine/default.aspx"
 
 // Create a static JSON and serve it
@@ -45,12 +44,9 @@ func mainRoute(c echo.Context) error {
 func newsRoute(c echo.Context) error {
 	news := make([]News, 0)
 
-	doc, err := goquery.NewDocument(URL)
-	if err != nil {
-		log.Fatal(err)
-	}
+	doc := getDocument()
 
-	doc.Find("#atm-comunicati div.news-item").Each(func(index int, item *goquery.Selection) {
+	doc.Find("#atm-comunicati div.news-item a").Each(func(index int, item *goquery.Selection) {
 		text := item.Text()
 		url, _ := item.Attr("href")
 
@@ -69,12 +65,9 @@ func newsRoute(c echo.Context) error {
 func statusRoute(c echo.Context) error {
 	lines := make([]LineStatus, 0)
 
-	doc, err := goquery.NewDocument(URL)
-	if err != nil {
-		log.Fatal(err)
-	}
+	doc := getDocument()
 
-	re := regexp.MustCompile(`/([M][0-9])\w/g`)
+	re := regexp.MustCompile(`[M][0-9]`)
 
 	doc.Find("#StatusLinee tr").Each(func(index int, item *goquery.Selection) {
 		line, _ := item.Find("div.StatusLinee_Stretch").Attr("id")
@@ -95,13 +88,42 @@ func statusRoute(c echo.Context) error {
 	return c.JSON(http.StatusOK, lines)
 }
 
+// handling the /trffic route
+func trafficRoute(c echo.Context) error {
+	traffic := make([]News, 0)
+
+	doc := getDocument()
+
+	doc.Find("#subhomepage-cx-infomobilita > div:nth-child(1) > table div.item.link-item a").Each(func(index int, item *goquery.Selection) {
+		text := item.Text()
+		url, _ := item.Attr("href")
+
+		entry := News{
+			Text: text,
+			Url:  url,
+		}
+
+		traffic = append(traffic, entry)
+	})
+
+	return c.JSON(http.StatusOK, traffic)
+}
+
+func getDocument() *goquery.Document {
+	doc, err := goquery.NewDocument(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+}
+
 // Server bootstrapping
 func main() {
-	e := echo.New()               // initialize Echo
-	e.GET("/", mainRoute)         // adding routes
-	e.GET("/news", newsRoute)     // adding routes
-	e.GET("/status", statusRoute) // adding routes
-	//e.GET("/traffic", trafficRoute) // adding routes
+	e := echo.New()                 // initialize Echo
+	e.GET("/", mainRoute)           // adding routes
+	e.GET("/news", newsRoute)       // adding routes
+	e.GET("/status", statusRoute)   // adding routes
+	e.GET("/traffic", trafficRoute) // adding routes
 
 	e.Logger.Fatal(e.Start(":8080")) // starting server
 }
